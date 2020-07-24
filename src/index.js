ccb922b989389ad90ec886b73b54453206e06eae
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-25 00:01:11 +0700

    layout products

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'fontawesome/css/font-awesome.css';
+import './theme-elements.css';
+import './skin.css';
+import './theme-shop.css';
+import './owl.carousel.min.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
