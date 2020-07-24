7df94b5309ecf7808bd470ccb0b84d1cdaf40c40
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-25 00:41:01 +0700

    create node server

---
+++ backend/data.js
@@ -0,0 +1,62 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      product_name: 'Slim Shirt',
+      category_id: '1',
+      image: '/images/d1.jpg',
+      product_price: 60,
+      product_description: ' Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '2',
+      product_name: 'Fit Shirt',
+      category_id: '2',
+      image: '/images/d1.jpg',
+      product_price: 50,
+      product_description: ' Nike',
+      rating: 4.2,
+      numReviews: 5
+    },
+    {
+      _id: '3',
+      product_name: 'Best Pants',
+      category_id: '1',
+      image: '/images/d1.jpg',
+      product_price: 70,
+      product_description: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    }, {
+      _id: '4',
+      product_name: 'Best Pants',
+      category_id: '3',
+      image: '/images/d1.jpg',
+      product_price: 70,
+      product_description: ' Nike',
+      rating: 4.5,
+      numReviews: 8
+    },
+    { _id: '5',
+      product_name: 'Slim Shirt',
+      category_id: '1',
+      image: '/images/d1.jpg',
+      product_price: 60,
+      product_description: ' Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: '6',
+      product_name: 'Fit Shirt',
+      category_id: '2',
+      image: '/images/d1.jpg',
+      product_price: 50,
+      product_description: ' Nike',
+      rating: 4.2,
+      numReviews: 5
+    },
+  ]
+}
