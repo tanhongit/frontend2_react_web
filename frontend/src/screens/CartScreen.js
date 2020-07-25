5c8855336b00fd747e5220e32f0714c3a4ac81d3
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-25 20:37:52 +0700

    add cookie cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -27,68 +27,68 @@ function CartScreen(props) {
         props.history.push('/signin?redirect=shipping');
     }
 
-    return (
-        <div className="cart">
-            <div className="cart-list">
-                <ul className="cart-list-container">
-                    <li>
-                        <h3>
-                            Shopping Cart
+    return <div className="cart">
+    <div className="cart-list">
+      <ul className="cart-list-container">
+        <li>
+          <h3>
+            Shopping Cart
           </h3>
-                        <div>
-                            Price
+          <div>
+            Price
           </div>
-                    </li>
-                    {
-                        cartItems.length === 0 ?
-                            <div>
-                                Cart is empty
+        </li>
+        {
+          cartItems.length === 0 ?
+            <div>
+              Cart is empty
           </div>
-                            :
-                            cartItems.map(item =>
-                                <li>
+            :
+            cartItems.map(item =>
+              <li>
+                <div className="cart-image">
+                  <img src={item.image} alt="product" />
+                </div>
+                <div className="cart-name">
+                  <div>
+                    <Link to={"/product/" + item.product}>
+                      {item.name}
+                    </Link>
 
-                                    <div className="cart-name">
-                                        <div>
-                                            <Link to={"/product/" + item.product}>
-                                                {item.name}
-                                            </Link>
-
-                                        </div>
-                                        <div>
-                                            Qty:
+                  </div>
+                  <div>
+                    Qty:
                   <select value={item.qty} onChange={(e) => dispatch(addToCart(item.product, e.target.value))}>
-                                                {[...Array(item.countInStock).keys()].map(x =>
-                                                    <option key={x + 1} value={x + 1}>{x + 1}</option>
-                                                )}
-                                            </select>
-                                            <button type="button" className="button" onClick={() => removeFromCartHandler(item.product)} >
-                                                Delete
+                      {[...Array(item.countInStock).keys()].map(x =>
+                        <option key={x + 1} value={x + 1}>{x + 1}</option>
+                      )}
+                    </select>
+                    <button type="button" className="button" onClick={() => removeFromCartHandler(item.product)} >
+                      Delete
                     </button>
-                                        </div>
-                                    </div>
-                                    <div className="cart-price">
-                                        ${item.price}
-                                    </div>
-                                </li>
-                            )
-                    }
-                </ul>
+                  </div>
+                </div>
+                <div className="cart-price">
+                  ${item.price}
+                </div>
+              </li>
+            )
+        }
+      </ul>
 
-            </div>
-            <div className="cart-action">
-                <h3>
-                    Subtotal ( {cartItems.reduce((a, c) => a + c.qty, 0)} items)
+    </div>
+    <div className="cart-action">
+      <h3>
+        Subtotal ( {cartItems.reduce((a, c) => a + c.qty, 0)} items)
         :
          $ {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
-                </h3>
-                <button onClick={checkoutHandler} className="button primary full-width" disabled={cartItems.length === 0}>
-                    Proceed to Checkout
+      </h3>
+      <button onClick={checkoutHandler} className="button primary full-width" disabled={cartItems.length === 0}>
+        Proceed to Checkout
       </button>
 
-            </div>
+    </div>
 
-        </div>
-    );
+  </div>
 }
 export default CartScreen;
