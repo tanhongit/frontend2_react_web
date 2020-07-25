d212ce1847431fa5dceed80b2df1a75f308aa69d
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-26 02:03:12 +0700

    create route signin

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,6 +3,25 @@ import User from '../models/userModel';
 
 const router = express.Router();
 
+
+router.post('/signin', async (req, res) => {
+  const signinUser = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (signinUser) {
+    res.send({
+      _id: signinUser.id,
+      name: signinUser.name,
+      email: signinUser.email,
+      isAdmin: signinUser.isAdmin,
+      token: getToken(signinUser),
+    });
+  } else {
+    res.status(401).send({ message: 'Invalid Email or Password.' });
+  }
+});
+
 router.get('/createadmin', async (req, res) => {
   try {
     const user = new User({
