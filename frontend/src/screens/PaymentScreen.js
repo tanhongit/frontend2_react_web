f249376ca768486f9328ca531acc138046ecbff9
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-26 21:49:01 +0700

    layout shipping & payment

--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -17,34 +17,42 @@ function PaymentScreen(props) {
   return (
     <div>
       <CheckoutSteps step1 step2 step3></CheckoutSteps>
-      <div className="form">
-        <form onSubmit={submitHandler}>
-          <ul className="form-container">
-            <li>
-              <h2>Payment</h2>
-            </li>
-
-            <li>
-              <div>
-                <input
-                  type="radio"
-                  name="paymentMethod"
-                  id="paymentMethod"
-                  value="paypal"
-                  onChange={(e) => setPaymentMethod(e.target.value)}
-                ></input>
-                <label for="paymentMethod">Paypal</label>
+      <main className="ps-main">
+        <div className="ps-contact ps-contact--2 ps-section pt-80 pb-80">
+          <div className="ps-container">
+            <div className="row">
+              <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+                <div className="ps-section__header pt-50">
+                  <h2 className="ps-section__title" data-mask="CHIKOIQUAN">
+                    - Payment
+            </h2>
+                  <form className="ps-contact__form" onSubmit={submitHandler} method="post">
+                    <h4>Choose your option</h4>
+                    <div className="form-group">
+                      <label>
+                        <b>Paypal</b> 
+                      </label>
+                      <input
+                        type="radio"
+                        name="paymentMethod"
+                        id="paymentMethod"
+                        value="paypal"
+                        onChange={(e) => setPaymentMethod(e.target.value)}
+                      ></input>
+                    </div>
+                    <div className="form-group">
+                      <button type="submit" className="ps-btn">
+                        Continue
+                  <i className="ps-icon-next" />
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
-            </li>
-
-            <li>
-              <button type="submit" className="button primary">
-                Continue
-              </button>
-            </li>
-          </ul>
-        </form>
-      </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -20,51 +20,53 @@ function ShippingScreen(props) {
   }
   return <div>
     <CheckoutSteps step1 step2 ></CheckoutSteps>
-    <div className="form">
-      <form onSubmit={submitHandler} >
-        <ul className="form-container">
-          <li>
-            <h2>Shipping</h2>
-          </li>
-
-          <li>
-            <label htmlFor="address">
-              Address
-          </label>
-            <input type="text" name="address" id="address" onChange={(e) => setAddress(e.target.value)}>
-            </input>
-          </li>
-          <li>
-            <label htmlFor="city">
-              City
-          </label>
-            <input type="text" name="city" id="city" onChange={(e) => setCity(e.target.value)}>
-            </input>
-          </li>
-          <li>
-            <label htmlFor="postalCode">
-              Postal Code
-          </label>
-            <input type="text" name="postalCode" id="postalCode" onChange={(e) => setPostalCode(e.target.value)}>
-            </input>
-          </li>
-          <li>
-            <label htmlFor="country">
-              Country
-          </label>
-            <input type="text" name="country" id="country" onChange={(e) => setCountry(e.target.value)}>
-            </input>
-          </li>
-
-
-          <li>
-            <button type="submit" className="button primary">Continue</button>
-          </li>
-
-        </ul>
-      </form>
-    </div>
+    <main className="ps-main">
+      <div className="ps-contact ps-contact--2 ps-section pt-80 pb-80">
+        <div className="ps-container">
+          <div className="row">
+            <div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 ">
+              <div className="ps-section__header pt-50">
+                <h2 className="ps-section__title" data-mask="CHIKOIQUAN">
+                  - Shipping
+        </h2>
+                <form className="ps-contact__form" onSubmit={submitHandler} method="post">
+                  <div className="form-group">
+                    <label>
+                      Address
+                    </label>
+                    <input name="address" id="address" className="form-control" onChange={(e) => setAddress(e.target.value)} type="text" placeholder />
+                  </div>
+                  <div className="form-group">
+                    <label>
+                      City
+                    </label>
+                    <input name="city" id="city" className="form-control" onChange={(e) => setCity(e.target.value)} type="text" placeholder />
+                  </div>
+                  <div className="form-group mb-25">
+                    <label>
+                      Postal Code
+                    </label>
+                    <input name="postalCode" id="postalCode" className="form-control" onChange={(e) => setPostalCode(e.target.value)} type="text" placeholder />
+                  </div>
+                  <div className="form-group mb-25">
+                    <label>
+                      Country
+                    </label>
+                    <input name="country" id="country" className="form-control" onChange={(e) => setCountry(e.target.value)} type="text" placeholder />
+                  </div>
+                  <div className="form-group">
+                    <button type="submit" className="ps-btn">
+                      Continue
+              <i className="ps-icon-next" />
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   </div>
-
 }
 export default ShippingScreen;
