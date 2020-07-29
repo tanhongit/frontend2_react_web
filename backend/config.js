8a9327d8836145cbbe6799ab1377cea71b426e6d
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-29 16:39:10 +0700

    none

--- backend/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export default{
-    MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/tanhong',
-    JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-}
