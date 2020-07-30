c59752ea8d3852e5cc0eb9ea889301526d193bba
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-30 21:02:42 +0700

    edit order detail screen

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -28,20 +28,20 @@ function OrderScreen(props) {
         <div className="placeorder-info">
           <div>
             <h3>
-              Shipping
+              <b>Shipping</b>
           </h3>
-            <div>
-              {order.shipping.address}, {order.shipping.city},
-          {order.shipping.postalCode}, {order.shipping.country},
+            <div style={{ paddingTop: 15 }}>
+             <b>Address:</b> {order.shipping.address}, {order.shipping.city},
+          {order.shipping.postalCode}, {order.shipping.country}.
           </div>
             <div>
               {order.isDelivered ? "Delivered at " + order.deliveredAt : "Not Delivered."}
             </div>
           </div>
           <div>
-            <h3>Payment</h3>
-            <div>
-              Payment Method: {order.payment.paymentMethod}
+            <h3><b>Payment</b></h3>
+            <div style={{ paddingTop: 15 }}>
+              Payment Method: <b>{order.payment.paymentMethod}</b>
             </div>
             <div>
               {order.isPaid ? "Paid at " + order.paidAt : "Not Paid."}
@@ -50,37 +50,32 @@ function OrderScreen(props) {
           <div>
             <ul className="cart-list-container">
               <li>
-                <h3>
-                  Shopping Cart
-          </h3>
+                <h3><b>Shopping Cart</b></h3>
                 <div>
-                  Price
-          </div>
+                  <b>Price</b>
+                </div>
               </li>
               {
                 order.orderItems.length === 0 ?
-                  <div>
-                    Cart is empty
-          </div>
+                  <div>Cart is empty</div>
                   :
                   order.orderItems.map(item =>
                     <li key={item._id}>
                       <div className="cart-image">
-                        <img src={item.image} alt="product" />
+                        <img src={"/images/products/" + item.image} width="50" alt="product" />
                       </div>
                       <div className="cart-name">
                         <div>
                           <Link to={"/product/" + item.product}>
                             {item.name}
                           </Link>
-
                         </div>
-                        <div>
-                          Qty: {item.qty}
+                        <div style={{textAlign:"center"}}>
+                          <b>Qty:</b> {item.qty}
                         </div>
                       </div>
                       <div className="cart-price">
-                        ${item.price}
+                        {item.price} VND
                       </div>
                     </li>
                   )
@@ -93,30 +88,26 @@ function OrderScreen(props) {
         <div className="placeorder-action">
           <ul>
             <li>
-              <h3>Order Summary</h3>
+              <h3><b>Order Summary</b></h3>
             </li>
             <li>
               <div>Items</div>
-              <div>${order.itemsPrice}</div>
+              <div>{order.itemsPrice} VND</div>
             </li>
             <li>
               <div>Shipping</div>
-              <div>${order.shippingPrice}</div>
+              <div>{order.shippingPrice} VND</div>
             </li>
             <li>
               <div>Tax</div>
-              <div>${order.taxPrice}</div>
+              <div>{order.taxPrice} VND</div>
             </li>
             <li>
-              <div>Order Total</div>
-              <div>${order.totalPrice}</div>
+              <div><b>Order Total</b></div>
+              <div><b>{order.totalPrice} VND</b></div>
             </li>
           </ul>
-
-
-
         </div>
-
       </div>
     </div>
 
