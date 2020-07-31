580e52e9213b0b1982d34ad1d8a5a4c22cf5a448
tanhongit <phuongtan12357nguyen@gmail.com>
2020-07-31 20:39:23 +0700

    add remove favorite item button

--- frontend/src/screens/FavoriteScreen.js
+++ frontend/src/screens/FavoriteScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { addToFavorite } from '../actions/favoriteAction';
+import { addToFavorite, removeFavorite } from '../actions/favoriteAction';
 
 function FavoriteScreen(props) {
 
@@ -19,6 +19,10 @@ function FavoriteScreen(props) {
     }
   }, []);
 
+  const removeFromFavoriteHandler = (productId) => {
+    dispatch(removeFavorite(productId));
+  }
+
   return (
     <>
       <main className="ps-main">
@@ -26,38 +30,40 @@ function FavoriteScreen(props) {
           <div style={{ textAlign: "center", paddingBottom: 20, fontWeight: "bold", fontSize: 40 }}>Favorite</div>
           <div className="ps-products" data-mh="product-listing">
             <div className="ps-product__columns">
-              {/* product */}
-              {favoriteItems.map(product =>
-                <div className="ps-product__column">
-                  <div className="ps-shoe mb-30">
-                    <div className="ps-shoe__thumbnail">
-                      <a className="ps-shoe__favorite" href={'/favorite/' + product.product}>
-                        <i className="ps-icon-heart" />
-                      </a>
-                      <Link to={'/product/' + product._id}><img src={"/images/products/" + product.image} alt='product' /></Link>
-                      <a className="ps-shoe__overlay" href={'/product/' + product.product} />
-                    </div>
-                    <div className="ps-shoe__content">
-                      <div class="ps-shoe__variants">
-                        {product.countInStock >= 1 ?
-                          <a href={"/cart/" + product.product + "?qty=1"} className='ps-btn btn btn-primary btn-add-to-cart-home'>Add to Cart</a>
-                          : <button disabled className="btn btn-info">Out of stock</button>}
-
-                      </div>
-                      <div className="ps-shoe__detail">
-                        <a className="ps-shoe__name" href={'/product/' + product.product}>
-                          {product.name}
-                        </a>
-                        <p className="ps-shoe__categories">
-                          Cate: <a href="#">{product.category}</a>, Brand: <a href="#">{product.brand}</a>.
+              {
+                favoriteItems.length === 0 ?
+                  <div style={{ textAlign: "center", paddingBottom: 20, fontWeight: "bold", fontSize: 40 }} className="cart-is-empty"><h2>Favorite is empty</h2></div>
+                  :
+                  favoriteItems.map(product =>
+                    <div className="ps-product__column">
+                      <div className="ps-shoe mb-30">
+                        <div className="ps-shoe__thumbnail">
+                          <a className="ps-shoe__favorite" onClick={() => removeFromFavoriteHandler(product.product)}>
+                            <i className="fa fa-times" />
+                          </a>
+                          <Link to={'/product/' + product._id}><img src={"/images/products/" + product.image} alt='product' /></Link>
+                          <a className="ps-shoe__overlay" href={'/product/' + product.product} />
+                        </div>
+                        <div className="ps-shoe__content">
+                          <div class="ps-shoe__variants">
+                            {product.countInStock >= 1 ?
+                              <a href={"/cart/" + product.product + "?qty=1"} className='ps-btn btn btn-primary btn-add-to-cart-home'>Add to Cart</a>
+                              : <button disabled className="btn btn-info">Out of stock</button>}
+                          </div>
+                          <div className="ps-shoe__detail">
+                            <a className="ps-shoe__name" href={'/product/' + product.product}>
+                              {product.name}
+                            </a>
+                            <p className="ps-shoe__categories">
+                              Cate: <a href="#">{product.category}</a>, Brand: <a href="#">{product.brand}</a>.
                         </p>
-                        <span style={{ paddingTop: 15 }} className="ps-shoe__price"> <Link to={'/product/' + product.product}>{product.price} Đ</Link></span>
+                            <span style={{ paddingTop: 15 }} className="ps-shoe__price"> <Link to={'/product/' + product.product}>{product.price} Đ</Link></span>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              )}
-              {/* end product */}
+                  )
+              }
             </div>
           </div>
         </div>
